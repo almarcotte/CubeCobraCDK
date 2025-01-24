be1764a13763267b7cfa61b211531db240563aa8
Alex Marcotte <alex.marcotte@gmail.com>
2025-01-24 16:02:02 -0500

    feat: adding environments

--- bin/cube_cobra_cdk.ts
+++ bin/cube_cobra_cdk.ts
@@ -1,44 +1,56 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { CubeCobraStack } from "../lib/CubeCobraStack";
-
-const envUSA = { account: "816705121310", region: "us-east-2" };
+import {CubeCobraStack} from "../lib/CubeCobraStack";
+import {environments} from "../config";
 
 const app = new cdk.App();
 
+const environment = app.node.tryGetContext('environment');
+const version = app.node.tryGetContext('version');
+
+if (!environment || !environments[environment]) {
+    throw new Error(`Invalid or missing environment. Available environments: ${Object.keys(environments).join(", ")}`);
+}
+
+if (!version || version.length === 0) {
+    throw new Error('Invalid or missing version');
+}
+
+const config = environments[environment];
+
 // Cubecon
 new CubeCobraStack(
-  app,
-  "CubeCobraDevStack",
-  {
-    accessKey: process.env.AWS_ACCESS_KEY_ID || "",
-    secretKey: process.env.AWS_SECRET_ACCESS_KEY || "",
-    emailUser: process.env.EMAIL_USER || "",
-    emailPass: process.env.EMAIL_PASS || "",
-    domain: "cubecobradev.com",
-    environmentName: "development",
-    version: "1.0.2",
-    awsLogGroup: "CUBECOBRA",
-    awsLogStream: "DEVELOPMENT",
-    dataBucket: "cubecobra-data-production",
-    downTimeActive: false,
-    dynamoPrefix: "PROD",
-    env: "production",
-    jobsToken: process.env.JOBS_TOKEN || "",
-    nitroPayEnabled: false,
-    patreonClientId: process.env.PATREON_CLIENT_ID || "",
-    patreonClientSecret: process.env.PATREON_CLIENT_SECRET || "",
-    patreonHookSecret: process.env.PATREON_HOOK_SECRET || "",
-    patreonRedirectUri: "https://cubecobra.com/patreon/redirect",
-    redisHost: process.env.REDIS_HOST || "",
-    sessionToken: process.env.SESSION_TOKEN || "",
-    sessionSecret: process.env.SESSION_SECRET || "",
-    tcgPlayerPublicKey: process.env.TCG_PLAYER_PUBLIC_KEY || "",
-    tcgPlayerPrivateKey: process.env.TCG_PLAYER_PRIVATE_KEY || "",
-    fleetSize: 1,
-  },
-  {
-    env: envUSA,
-  }
+    app,
+    "CubeCobraDevStack",
+    {
+        accessKey: process.env.AWS_ACCESS_KEY_ID || "",
+        secretKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+        emailUser: process.env.EMAIL_USER || "",
+        emailPass: process.env.EMAIL_PASS || "",
+        domain: config.domain,
+        environmentName: environment,
+        version: version,
+        awsLogGroup: config.awsLogGroup,
+        awsLogStream: config.awsLogStream,
+        dataBucket: config.dataBucket,
+        downTimeActive: config.downTimeActive,
+        dynamoPrefix: config.dynamoPrefix,
+        env: environment,
+        jobsToken: process.env.JOBS_TOKEN || "",
+        nitroPayEnabled: config.nitroPayEnabled,
+        patreonClientId: process.env.PATREON_CLIENT_ID || "",
+        patreonClientSecret: process.env.PATREON_CLIENT_SECRET || "",
+        patreonHookSecret: process.env.PATREON_HOOK_SECRET || "",
+        patreonRedirectUri: config.patreonRedirectUri,
+        redisHost: process.env.REDIS_HOST || "",
+        sessionToken: process.env.SESSION_TOKEN || "",
+        sessionSecret: process.env.SESSION_SECRET || "",
+        tcgPlayerPublicKey: process.env.TCG_PLAYER_PUBLIC_KEY || "",
+        tcgPlayerPrivateKey: process.env.TCG_PLAYER_PRIVATE_KEY || "",
+        fleetSize: config.fleetSize,
+    },
+    {
+        env: {account: config.account, region: config.region,},
+    }
 );
---
+++ config.ts
@@ -0,0 +1,43 @@
+export interface EnvironmentConfiguration {
+    account: string;
+    region: string;
+    dataBucket: string;
+    domain: string;
+    awsLogGroup: string;
+    awsLogStream: string;
+    downTimeActive: boolean;
+    dynamoPrefix: string;
+    nitroPayEnabled: boolean;
+    patreonRedirectUri: string;
+    fleetSize: number;
+}
+
+export const environments: { [key: string]: EnvironmentConfiguration } = {
+    development: {
+        account: '',
+        region: 'us-east-2',
+        dataBucket: 'cubecobra-data-production',
+        domain: 'cubecobradev.com',
+        awsLogGroup: 'CUBECOBRA',
+        awsLogStream: 'DEVELOPMENT',
+        downTimeActive: false,
+        dynamoPrefix: "DEV",
+        nitroPayEnabled: false,
+        patreonRedirectUri: 'https://cubecobradev.com/patreon/redirect',
+        fleetSize: 1,
+    },
+
+    production: {
+        account: '816705121310',
+        region: 'us-east-2',
+        dataBucket: 'cubecobra-data-production',
+        domain: 'cubecobra.com',
+        awsLogGroup: 'CUBECOBRA',
+        awsLogStream: 'PRODUCTION',
+        downTimeActive: false,
+        dynamoPrefix: 'PROD',
+        nitroPayEnabled: true,
+        patreonRedirectUri: 'https://cubecobra.com/patreon/redirect',
+        fleetSize: 1,
+    }
+}
